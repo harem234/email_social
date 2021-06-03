34ace6e9649bb46d393ed305756506c55eb0393e
mehdi hamzeh <ssdhdd85@gmail.com>
2021-06-03 09:00:57 +0430

    release file fix, add postgres, set heroku setting in django settings

--- release.bash
+++ release.bash
@@ -1,6 +1,6 @@
-# python -m pip install pip-tools && \
-# pip-compile ./requirements.in && \
-# pip-sync ./requirements.txt && \
+python -m pip install pip-tools && \
+pip-compile ./requirements.in && \
+pip-sync ./requirements.txt && \
 python manage.py collectstatic --clear --no-input && \
 python manage.py compress --force && \
 python manage.py collectstatic --no-post-process --no-input && \
