83b5e7ada39f43fd4ea2957b34d053e7f9b0e7a3
mehdi hamzeh <ssdhdd85@gmail.com>
2021-06-03 10:36:54 +0430

    staticfiles

--- release.bash
+++ release.bash
@@ -1,8 +1,8 @@
 # python -m pip install pip-tools && \
 # pip-compile ./requirements.in && \
 # pip-sync ./requirements.txt && \
-# python manage.py collectstatic --clear --no-input && \
-# python manage.py compress --force && \
-# python manage.py collectstatic --no-post-process --no-input && \
+python manage.py collectstatic --clear --no-input && \
+python manage.py compress --force && \
+python manage.py collectstatic --no-post-process --no-input && \
 
 python manage.py migrate
