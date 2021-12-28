5803da25f146e74f61bc4dfbcb86b0a7d84728cf
mehdi hamzeh <ssdhdd85@gmail.com>
2021-12-28 13:24:34 +0330

    pgbouncer in procfile issue

--- DJ_EmailUser_Social/settings/deploy.py
+++ DJ_EmailUser_Social/settings/deploy.py
@@ -96,14 +96,14 @@ EMAIL_USE_TLS = True
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
